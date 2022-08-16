1d4f568da113f95af3eb9100ef29c36bc230319a
TuanPhong131 <matuanphong98@gmail.com>
2022-08-16 16:41:21 +0700

    update

---
+++ src/main/java/com/example/FruitsStore_BE/FruitsStoreBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.FruitsStore_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitsStoreBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitsStoreBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FruitsStore_BE/FruitsStoreBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FruitsStore_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitsStoreBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
